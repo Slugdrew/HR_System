666c398b4d4bf2ccac28a4bf2242bf0b59ec42dc
Slugdrew <andrewbh80@gmail.com>
2021-05-24 19:51:04 -0700

    Initial Commit

---
+++ HR_System.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# ------------------------------------------#
+# Title: HR_System.py
+# Desc: Assignment HR System - Create the HR System application
+# Change Log: (Who, When, What)
+# AHernandez, 2021-May-24, Created File for HR System application and skeletal structure 
+# ------------------------------------------#
+strChoice = '' 
+lstTbl = []
+dicRow = {}
+
+
+
+# -- PROCESSING -- #
+class DataProcessor:
+    pass
+
+class FileProcessor:
+    pass
+
+
+
+
+# -- Input/Output -- #
+
+class IO:
+    pass
+
+
+# -- PRESENTATION (Input/Output) -- #
+class Presentation:
+    pass

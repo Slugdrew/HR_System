aaba10f51abab3ea8a927cf416a7475eded2e51c
Slugdrew <andrewbh80@gmail.com>
2021-05-26 21:08:46 -0700

    Added import CSV functionality and more Error handeling. Also cleaned up unused code

--- HR_System.py
+++ HR_System.py
@@ -8,17 +8,18 @@
 # AHernandez, 2021-May-25, Added data structure and input functions 
 # AHernandez, 2021-May-25, Added entering new user, exporting to csv, and printing table
 # AHernandez, 2021-May-25, added exporting table to text as a report  
+# AHernandez, 2021-May-25, Added importing file functionality  
 # ------------------------------------------#
 import time
 import re
 from datetime import datetime 
 import pandas as pd
-#import pathlib
+import pathlib
 #import numpy as np
 import csv
 from prettytable import PrettyTable
 strChoice = '' 
-#lstTbl = []
+lstTbl = []
 dictRow = {}
 menuOptions = '\nMenu:\n\t[R] Create a Report\n\t[L] Load Employee File\n\t[S] Save Current List\n\t[C] Current Employees \
                 \n\t[P] Previous Employees\n\t[A] Add Employee\n\t[E] Edit Current Employee\n\t[X] Quit'
@@ -37,7 +38,7 @@ class DataProcessor:
     def find_current_employee(dictRow):
         try:
             for key, val in dictRow.items():
-                if datetime.now() < datetime.strptime(val[6], '%Y-%m-%d'):
+                if datetime.today().strftime('%Y-%m-%d') <  val[6] :
                     table = PrettyTable(fieldnames)
                     for key, val in dictRow.items():
                        table.add_row([key, *val])
@@ -57,18 +58,16 @@ class DataProcessor:
             print('The table is empty.  There is nothing to print.')
 # -- FILE PROCESSING -- #         
 class FileProcessor:
-    def read_file(file_name, dictRow):
+    def read_file(file_name, dictRow,lstTbl):
+        dictRow.clear()
         try:
-            with open(file_name, mode='r') as infile:
-                reader = csv.reader(infile)
-                dictRow = {rows[0]:rows[1] for rows in reader}
-            print(dictRow)
-# ==============================THIS WORKS NEED TO REMOVE HEADER BUT PANDAS MIGHT BE BETTER===============================================
-#             with open(file_name) as file:
-#             reader = csv.reader(file)
-#             dictRow = {rows[0]:rows[1] for rows in reader}
-#             print(dictRow)
-# =============================================================================
+            with open(file_name, mode='r') as file:
+                next(file)
+                for line in csv.reader(file):
+                    tup = line[1],line[2],line[3],line[4],line[5],line[6],line[7]
+                    lstTbl = list(tup)
+                    dictRow[line[0]] = lstTbl
+
         except (FileNotFoundError,OSError):
             print('There is no file with that name.')
         return dictRow
@@ -77,7 +76,6 @@ class FileProcessor:
         strOpt = Presentation.file_status()
         if strOpt == 'yes':
             try:
-                #columnNames = ['Name', 'Address', 'SSN', 'DOB', 'jobTitle', 'startDate', 'EndDate']
                 (pd.DataFrame.from_dict(data=dictRow, orient='index',)
                  .to_csv(file_name, mode='w'))
             except ValueError:
@@ -85,27 +83,7 @@ class FileProcessor:
             except PermissionError:
                 print('The file is open, Please close and try again.')
         return None
-# ===================================THIS WORKS==========================================
-#         with open(file_name, 'w') as file:  
-#             writer = csv.writer(file)
-#             for key, value in dictRow.items():
-#                writer.writerow([key, value])
-# =============================================================================
-# =============================================================================
-#         fieldnames = ['EmpId','Name', 'Address', 'SSN', 'DOB', 'jobTitle', 'startDate', 'EndDate']
-#         with open(file_name,'w') as file:
-#             writer = csv.DictWriter(file, fieldnames=fieldnames)
-#             writer.writeheader()
-# =============================================================================
- 
-# =============================================================================
-#         with open(file_name,'w', encoding='UTF8', newline='') as file:
-#             writer = csv.DictWriter(file, fieldnames=fieldnames)
-#             writer.writeheader()
-#             writer.writerow(dictRow)
-#             writer.writerows(zip(*[dictRow[row] for row in dictRow]))
-# =============================================================================
-    
+
     def export_current_table(tblex, table):
         if tblex == 'yes':
             with open('CurrentEmployeeTable.txt', 'w') as table_export:
@@ -161,9 +139,13 @@ class IO:
                 break
             except ValueError:
                 print("Incorrect Start Date format, It should be YYYY-MM-DD") 
-        dateEndDate = '2100-01-01'                  
+        dateEndDate = '2100-01-01'                 
         return strName, strAddress, strSSN ,dateDOB, strJobTitle, dateStartDate, dateEndDate, empId
     
+    def export_to_text():
+        tblExport = input('Would you like to export this table? (Yes/No) ').strip().lower()
+        return tblExport
+        
 # -- PRESENTATION -- #
 class Presentation:    
     def file_status():
@@ -178,26 +160,23 @@ class Presentation:
     def show_current_employee(table):
         if table != None:
             print (table)                
-            tblExport = input('Would you like to export this table? (Yes/No) ').strip().lower()
-            return tblExport
     
     def show_previous_employee(table):
         if table != None:
             print (table)                
-            tblExport = input('Would you like to export this table? (Yes/No) ').strip().lower()
-            return tblExport
+
+    def ask_change_emp():
+        pass
 # -- Main -- #
 class Main:    
     def main():
         global intEmpId
         intEmpId = 1
-# =============================================================================
-#         file =  pathlib.Path(strFileName)
-#         if file.exists():
-#             FileProcessor.read_file(strFileName, dictRow)
-#         else:
-#             IO.file_state(strFileName)
-# =============================================================================
+        file =  pathlib.Path(strFileName)
+        if file.exists():
+            FileProcessor.read_file(strFileName, dictRow,lstTbl)
+        else:
+            Presentation.file_state(strFileName)
         try:
             while True:
                 print(menuOptions)
@@ -208,18 +187,25 @@ class Main:
                     strName, strAddress, strSSN ,dateDOB, strJobTitle, dateStartDate,dateEndDate, intEmpId = IO.add_new_employee(intEmpId)
                     DataProcessor.append_employee(strName, strAddress, strSSN 
                                                   ,dateDOB, strJobTitle, dateStartDate,dateEndDate, intEmpId)
+                    print(dictRow)
                 elif strChoice == 's':
                     FileProcessor.write_file(strFileName, dictRow)
                 elif strChoice == 'l':
-                    FileProcessor.read_file(strFileName, dictRow)
+                    FileProcessor.read_file(strFileName, dictRow, lstTbl)
                 elif strChoice == 'c':
                     ptable = DataProcessor.find_current_employee(dictRow)
-                    tblExport = Presentation.show_current_employee(ptable)
-                    FileProcessor.export_current_table(tblExport,ptable)
+                    Presentation.show_current_employee(ptable)
+                    if ptable != None:
+                        tblExport = IO.export_to_text()
+                        FileProcessor.export_current_table(tblExport,ptable)
                 elif strChoice == 'p':
                     ptable = DataProcessor.find_previous_employee(dictRow)
-                    tblExport = Presentation.show_previous_employee(ptable)
-                    FileProcessor.export_previous_table(tblExport,ptable)
+                    Presentation.show_previous_employee(ptable)
+                    if ptable != None:
+                        tblExport = IO.export_to_text()
+                        FileProcessor.export_previous_table(tblExport,ptable)
+                elif strChoice == 'e':
+                    pass
         except KeyboardInterrupt:
             print('\nThe user has caused a keyboard interruption\nThe program will now close!')
             time.sleep(2)
